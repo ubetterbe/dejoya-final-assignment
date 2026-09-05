60914cc1cdfaca29c0158b07612bd58e4f94185f
TonyStark-byte1 <ckdejoya@gmail.com>
2026-09-05 23:15:17 +1000

    Initial project setup
    Default Android Studio Empty Activity template. Kotlin, API 27 minimum, Kotlin DSL build config. Starting point before implementing Login, Dashboard, and Details screens.

---
+++ app/src/main/java/com/example/dejoyafinalassessment/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.dejoyafinalassessment
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.dejoyafinalassessment.ui.theme.DeJoyaFinalAssessmentTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            DeJoyaFinalAssessmentTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    DeJoyaFinalAssessmentTheme {
+        Greeting("Android")
+    }
+}
